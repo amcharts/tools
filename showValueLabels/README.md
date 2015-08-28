9e0b88d7e18fb4f73c3e42bd13f105a07fc195c8
Pauan <pcxunlimited@gmail.com>
2015-08-27 23:06:55 -1000

    Added showValueLabels plugin

---
+++ showValueLabels/README.md
@@ -0,0 +1,56 @@
+# amCharts Plugin: Show Value Labels
+
+Version: 1.0.0
+
+
+## Description
+
+For each grid line, displays a label which shows the current value.
+
+Works only with Stock charts.
+
+## Installation
+
+To enable, include `showValueLabels.min.js` on your web page.
+
+I.e.:
+
+```
+<script src="//www.amcharts.com/lib/3/plugins/tools/showValueLabels/showValueLabels.min.js"></script>
+```
+
+## Usage
+
+To enable, set `showValueLabels` property to `true` in your `stockGraphs`:
+
+```
+"stockGraphs": [{
+  "showValueLabels": true
+}]
+```
+
+
+## License
+
+All software included in this collection is licensed under Apache License 2.0.
+
+This basically means you're free to use or modify it, even make your own
+versions or completely different products out of them.
+
+Please see attached file "license.txt" for the complete license or online here:
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+
+## Contact us
+
+* Email:contact@amcharts.com
+* Web: http://www.amcharts.com/
+* Facebook: https://www.facebook.com/amcharts
+* Twitter: https://twitter.com/amcharts
+
+
+## Changelog
+
+### 1.0
+* Initial release
---
+++ showValueLabels/showValueLabels.js
@@ -0,0 +1,108 @@
+/*
+Plugin Name: amCharts Show Value Labels
+Description: Shows the current value in a label above each grid line
+Author: Paul Chapman, amCharts
+Version: 1.0
+Author URI: http://www.amcharts.com/
+
+Copyright 2015 amCharts
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Please note that the above license covers only this plugin. It by all means does
+not apply to any other amCharts products that are covered by different licenses.
+*/
+
+/* globals AmCharts */
+/* jshint -W061 */
+
+AmCharts.addInitHandler( function( chart ) {
+	var seen = false;
+
+	// Fix this later
+	function validate( chart ) {
+		seen = true;
+
+		try {
+			chart.validateNow( false, true );
+		} finally {
+			seen = false;
+		}
+	}
+
+	function getPeriod( graph ) {
+		return "value" + graph.periodValue;
+	}
+
+	function processGraph( guides, graph ) {
+		if ( graph.showValueLabels ) {
+			var data = graph.data;
+
+			var type = getPeriod( graph );
+
+			for ( var i = 0; i < data.length; ++i ) {
+				var info = data[ i ].dataContext;
+
+				guides.push( {
+					"date": info.date,
+					"label": info[ type ],
+					"lineAlpha": 0,
+					"inside": true,
+					"labelRotation": 90
+				} );
+			}
+
+			return true;
+		}
+	}
+
+	function processPanel( panel ) {
+		var graphs = panel.stockGraphs;
+		var categoryAxis = panel.categoryAxis;
+		var guides = categoryAxis.guides;
+
+		guides.length = 0;
+
+		var processed = false;
+
+		for ( var i = 0; i < graphs.length; ++i ) {
+			if ( processGraph( guides, graphs[ i ] ) ) {
+				processed = true;
+			}
+		}
+
+		return processed;
+	}
+
+	function processPanels( chart ) {
+		var panels = chart.panels;
+
+		var processed = false;
+
+		for ( var i = 0; i < panels.length; ++i ) {
+			if ( processPanel( panels[ i ] ) ) {
+				processed = true;
+			}
+		}
+
+		return processed;
+	}
+
+	chart.addListener( "zoomed", function() {
+		if ( !seen ) {
+			if ( processPanels( chart ) ) {
+				validate( chart );
+			}
+		}
+	} );
+}, [ "stock" ] );
