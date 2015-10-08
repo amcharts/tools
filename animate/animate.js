a8a2ab099dab3ccfe7e3d80f2d900e41085d69e5
Pauan <pcxunlimited@gmail.com>
2015-10-07 16:17:48 -1000

    Lots of work on the animate plugin

--- animate/animate.js
+++ animate/animate.js
@@ -26,10 +26,249 @@ not apply to any other amCharts products that are covered by different licenses.
 /* globals AmCharts */
 /* jshint -W061 */
 
-AmCharts.addInitHandler( function( chart ) {
+(function() {
+	"use strict";
 
-	chart.animateData = function( data, options ) {
-		options.complete();
+
+	function raf( self ) {
+		requestAnimationFrame( function( now ) {
+			self._onFrame( now );
+		} );
+	}
+
+
+	function noop( now ) {}
+
+
+	function Animation( duration, start, tick, end ) {
+		this._finished = false;
+		this._startTime = null;
+		this._duration = duration;
+		this._start = (start == null ? noop : start);
+		this._tick = (tick == null ? noop : tick);
+		this._end = (end == null ? noop : end);
+	}
+
+	Animation.prototype.cancel = function() {
+		this._finished = true;
+		this._startTime = null;
+		this._duration = null;
+		this._start = null;
+		this._tick = null;
+		this._end = null;
 	};
 
-}, [ "serial" ] );
+	Animation.prototype._onFrame = function( now ) {
+		// This will only happen when the animation was cancelled
+		if ( this._finished ) {
+			return true;
+
+		} else if ( this._startTime === null ) {
+			this._startTime = now;
+			this._start( 0 );
+			return false;
+
+		} else {
+			var diff = now - this._startTime;
+
+			if ( diff < this._duration ) {
+				this._tick( diff / this._duration );
+				return false;
+
+			} else {
+				this._end( 1 );
+				// Cleanup all the properties
+				this.cancel();
+				return true;
+			}
+		}
+	};
+
+
+	function Animator() {
+		this._animating = false;
+		this._animations = [];
+		this._onBeforeFrames = [];
+		this._onAfterFrames = [];
+	}
+
+
+	Animator.prototype.animate = function( options ) {
+		var animation = new Animation(
+			options.duration,
+			options.start,
+			options.tick,
+			options.end
+		);
+
+		this._animations.push( animation );
+
+		if ( !this._animating ) {
+			this._animating = true;
+
+			raf( this );
+		}
+
+		return animation;
+	};
+
+
+	Animator.prototype.onBeforeFrame = function( f ) {
+		this._onBeforeFrames.push( f );
+	};
+
+	Animator.prototype.onAfterFrame = function( f ) {
+		this._onAfterFrames.push( f );
+	};
+
+
+	Animator.prototype._onFrame = function( now ) {
+		var onBeforeFrames = this._onBeforeFrames;
+
+		for ( var i = 0; i < onBeforeFrames.length; ++i ) {
+			onBeforeFrames[ i ]( now );
+		}
+
+
+		var animations = this._animations;
+
+		for ( var i = 0; i < animations.length; ++i ) {
+			var animation = animations[ i ];
+
+			// If the animation is finished...
+			if ( animation._onFrame( now ) ) {
+				// TODO this is a bit slow, but I don't know of a faster alternative
+				animations.splice( i, 1 );
+				--i;
+			}
+		}
+
+
+		var onAfterFrames = this._onAfterFrames;
+
+		for ( var i = 0; i < onAfterFrames.length; ++i ) {
+			onAfterFrames[ i ]( now );
+		}
+
+
+		// All animations are finished
+		if ( animations.length === 0 ) {
+			this._animating = false;
+
+		} else {
+			raf( this );
+		}
+	};
+
+
+	var _animator = new Animator();
+
+	AmCharts.animate = function( options ) {
+		return _animator.animate( options );
+	};
+
+	AmCharts.tween = function( time, from, to ) {
+		return ( time * ( to - from ) ) + from;
+	};
+
+
+	function easeInOut3( t ) {
+		var r = ( t < 0.5 ? t * 2 : ( 1 - t ) * 2 );
+		r *= r * r * r;
+		return ( t < 0.5 ? r / 2 : 1 - ( r / 2 ) );
+	}
+
+	function easeIn3( t ) {
+		t *= t * t * t;
+		return t;
+	}
+
+	function easeOut3( t ) {
+		var r = ( 1 - t );
+		r *= r * r * r;
+		return ( 1 - r );
+	}
+
+
+	var needsValidation = [];
+
+	function map( array, fn ) {
+		var length = array.length;
+		var output = new Array( length );
+
+		for ( var i = 0; i < length; ++i ) {
+			output[ i ] = fn( array[ i ] );
+		}
+
+		return output;
+	}
+
+	function pushNew( array, x ) {
+		for ( var i = 0; i < array.length; ++i ) {
+			if ( array[ i ] === x ) {
+				return;
+			}
+		}
+
+		array.push( x );
+	}
+
+	// TODO check the performance of this
+	_animator.onAfterFrame( function() {
+		for ( var i = 0; i < needsValidation.length; ++i ) {
+			needsValidation[ i ].validateData();
+		}
+
+		needsValidation.length = 0;
+	} );
+
+
+
+	function animateData( data, options ) {
+		var self = this;
+
+		var property = options.property;
+
+		var dataProvider = self.dataProvider;
+
+		function get( x ) {
+			return x[ property ];
+		}
+
+		var oldData = map( dataProvider, get );
+		var newData = map( data, get );
+
+		function tick( time ) {
+			time = easeOut3( time );
+
+			for ( var i = 0; i < newData.length; ++i ) {
+				dataProvider[ i ][ property ] = AmCharts.tween(
+					time,
+					oldData[ i ],
+					newData[ i ]
+				);
+			}
+
+			// TODO check the performance of this
+			pushNew(needsValidation, self);
+		}
+
+		return AmCharts.animate({
+			duration: options.duration,
+			tick: tick,
+			end: function ( time ) {
+				tick( time );
+
+				if ( options.complete != null ) {
+					options.complete();
+				}
+			}
+		});
+	}
+
+
+	AmCharts.addInitHandler( function( chart ) {
+		chart.animateData = animateData;
+	}, [ "serial" ] );
+
+})();
